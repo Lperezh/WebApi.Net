0ec7f2d8314ca4a6eb30d5a902bfd46eec2ea91e
Leonardo Perez <acrossmovil@gmail.com>
2017-11-10 09:05:47 -0300

    token

---
+++ WebApi/App_Start/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApi.App_Start.Startup1))]
+
+namespace WebApi.App_Start
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Para obtener más información sobre cómo configurar la aplicación, visite https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
---
+++ WebApiTokenAuthentication/Controllers/FormsController.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiTokenAuthentication.servicio;
+
+namespace WebApiTokenAuthentication.Controllers
+{
+    [Authorize]
+    public class FormsController : ApiController
+    {
+       
+        public HttpResponseMessage getforms()
+        {
+
+            FormsClient servicio = new FormsClient();
+
+            FormsResponseFUN response = new FormsResponseFUN();
+            List<FormsFUN> listaEntidad = new List<FormsFUN>();
+            HttpResponseMessage respuesta = new HttpResponseMessage();
+
+
+            bool BadRequest = false;
+
+            //token de validacion
+            // if (token == "null" || token == string.Empty || token == null) { BadRequest = true; };
+            string validaToken = "E_00";
+
+            if (!BadRequest)
+            {
+                if (validaToken == "E_00")
+                {
+
+                    response = servicio.ObtenerForms();
+                    if (response.status == 200)
+                    {
+
+                        respuesta.StatusCode = HttpStatusCode.OK;
+                        respuesta.Content = new StringContent(JsonConvert.SerializeObject(response));
+                    }
+                    else
+                    {
+                        respuesta.StatusCode = HttpStatusCode.InternalServerError;
+                    }
+
+                }
+                else
+                {
+                    respuesta.StatusCode = HttpStatusCode.Unauthorized;
+
+                }
+
+            }
+            else
+            {
+                respuesta.StatusCode = HttpStatusCode.BadRequest;
+            }
+
+            return respuesta;
+
+        }
+    }
+}
